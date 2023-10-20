6762f6d0f8ef567b0880f39094137dcbaa4ca774
oscarabrego16 <00206019@uca.edu.sv>
2023-10-20 11:56:56 -0600

    Modify: test

--- src/main/java/org/oscarrdrgz/jenkins/Main.java
+++ src/main/java/org/oscarrdrgz/jenkins/Main.java
@@ -6,8 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
+        System.out.printf("Hola jenkins!");
 
     }
 }
